3d5b432e52576d19e439d729786e31aaa947c2a2
Mac <redsbaniasadi1381@gmail.com>
2024-01-16 11:03:00 +0000

    get crnn model

--- src/train.py
+++ src/train.py
@@ -12,3 +12,16 @@ from torch.nn import CTCLoss
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor
 
 torch.backends.cudnn.benchmark = True
+
+
+class LitCRNN(pl.LightningModule):
+    def __init__(self, img_h, n_channels, n_classes, n_hidden, lstm_input, lr, lr_reduce_factor, lr_patience, min_lr):
+        super(LitCRNN, self).__init__()
+        self.save_hyperparameters()
+        self.model = CRNNModelTorch(img_h=self.hparams.img_h,
+                                    n_channels=self.hparams.n_channels,
+                                    n_classes=self.hparams.n_classes,
+                                    n_hidden=self.hparams.n_hidden,
+                                    lstm_input=self.hparams.lstm_input)
+        self.model.apply(self.model.weights_init)
+        self.criterion = CTCLoss(reduction='mean')
