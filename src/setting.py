d4c8a1a529524c97a79882cb17b0a9ec14c4de4c
Mac <redsbaniasadi1381@gmail.com>
2024-02-04 01:59:00 +0000

    modify

--- src/setting.py
+++ src/setting.py
@@ -1,7 +1,24 @@
 import torch
-# from torchvision import transforms
+from torchvision import transforms
 import albumentations as A
 from dataclasses import dataclass
 from albumentations.pytorch import ToTensorV2
 from alphabets import ALPHABETS
 from argparse import Namespace
+
+
+@dataclass(init=True)
+class BasicConfig:
+    img_h = 32 
+    img_w = 100  
+
+    file_name = "best"
+
+    # Modify
+    n_classes = 35
+    mean = [0.4845]
+    std = [0.1884]
+    alphabet_name = "FA_LPR"
+    train_directory = '/home/ai/projects/vehicle-plate-recognition-training/recognition/datasets/train'
+    val_directory = '/home/ai/projects/vehicle-plate-recognition-training/recognition/datasets/val'
+    output_dir = "output"
