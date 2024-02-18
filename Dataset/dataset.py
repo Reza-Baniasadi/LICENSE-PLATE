1ec4587a242d70c16c6c74d8a64db4019fa95503
Mac <redsbaniasadi1381@gmail.com>
2024-02-18 08:45:00 +0000

    add atribute

--- Dataset/dataset.py
+++ Dataset/dataset.py
@@ -13,3 +13,15 @@ from torch import nn
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 import numpy as np
+
+
+class CRNNDataset(Dataset):
+
+    def __init__(self, root, characters, transform=None, logger=None):
+        self.transform = transform
+        self.char2label = {char: i + 1 for i, char in enumerate(characters)}
+        self.label2char = {label: char for char, label in self.char2label.items()}
+        self.image_paths, self.labels, self.labels_length = self.get_image_paths(root, characters,
+                                                                                 chars2label=self.char2label,
+                                                                                 logger=logger)
+        self.n_classes = len(self.label2char) + 1  
