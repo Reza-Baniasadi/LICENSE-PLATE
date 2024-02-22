d110221990a414181cb1f490579c7cafbc1d1dde
Mac <redsbaniasadi1381@gmail.com>
2024-02-22 01:24:00 +0000

    function get item add and debug

--- Dataset/dataset.py
+++ Dataset/dataset.py
@@ -61,3 +61,18 @@ class CRNNDataset(Dataset):
     def __len__(self):
         return len(self.image_paths)
     
+    def __getitem__(self, index):
+        assert index <= len(self), 'index range error'
+        img_path = self.image_paths[index]
+        if isinstance(self.transform, albumentations.core.composition.Compose):
+            img = cv2.imread(img_path)[..., ::-1]  
+            img = np.array(Image.open(img_path))[..., :3]
+            img = self.transform(image=img)['image'][0:1, ...].unsqueeze(0)  
+        else:
+            img = Image.fromarray(np.array(Image.open(img_path))[..., :3])
+            img = self.transform(img).unsqueeze(0)  
+        label = torch.LongTensor(self.labels[index]).unsqueeze(0)
+        label_length = torch.LongTensor([self.labels_length[index]]).unsqueeze(0)
+
+        return img, label, label_length
+    
