ac90a8d86267e85e64e3d9348307e0637bcaaa5c
Mac <redsbaniasadi1381@gmail.com>
2024-03-03 09:14:03 +0000

    for utils

--- src/train.py
+++ src/train.py
@@ -38,3 +38,14 @@ class Config:
 
     def update_config_param(self, args):
         self.__dict__.update(vars(args))
+
+
+    def mkdir_incremental(path: Path):
+        path = Path(path)
+        i = 0
+        new_path = path
+        while new_path.exists():
+            i += 1
+            new_path = path.parent / f"{path.name}_{i}"
+        new_path.mkdir(parents=True, exist_ok=True)
+        return new_path
