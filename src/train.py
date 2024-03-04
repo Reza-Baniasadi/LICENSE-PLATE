b0de3dea01e1a30a562c1c7335bf9967f56d4ebb
Mac <redsbaniasadi1381@gmail.com>
2024-03-04 10:44:03 +0000

    add logger

--- src/train.py
+++ src/train.py
@@ -49,3 +49,22 @@ class Config:
             new_path = path.parent / f"{path.name}_{i}"
         new_path.mkdir(parents=True, exist_ok=True)
         return new_path
+    
+    def get_logger(name, log_path=None):
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.INFO)
+
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+        if log_path:
+            fh = logging.FileHandler(log_path)
+            fh.setLevel(logging.INFO)
+            fh.setFormatter(formatter)
+            logger.addHandler(fh)
+
+        return logger
