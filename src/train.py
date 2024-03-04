df8d8d11389488b6424177d04ddb751025098eb8
Mac <redsbaniasadi1381@gmail.com>
2024-03-04 10:34:00 +0000

    visualize data

--- src/train.py
+++ src/train.py
@@ -68,3 +68,13 @@ class Config:
             logger.addHandler(fh)
 
         return logger
+    
+
+    def visualize_data_loader(loader, mean, std):
+        imgs, labels = next(iter(loader))
+        imgs = imgs[:8]
+        imgs = imgs * torch.tensor(std).view(-1, 1, 1) + torch.tensor(mean).view(-1, 1, 1)
+
+        grid = torchvision.utils.make_grid(imgs, nrow=4)
+        plt.imshow(grid.permute(1, 2, 0))
+        plt.show()
