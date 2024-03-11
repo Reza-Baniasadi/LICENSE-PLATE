62e9572de69aa1c2f2b0f9ff1383f979b4a06e82
Mac <redsbaniasadi1381@gmail.com>
2024-03-11 03:04:00 +0000

    git main add

--- src/train.py
+++ src/train.py
@@ -147,3 +147,66 @@ class LitCRNN(pl.LightningModule):
             val_loader = DataLoader(val_dataset, batch_size=config.batch_size,
                                     shuffle=False, num_workers=config.n_workers)
             return train_loader, val_loader
+
+
+# ---------------- Main ----------------
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--train_directory", type=Path,
+                        default="/home/ai/projects/vehicle-plate-recognition-training/recognition/datasets/train",
+                        help="path to the dataset, default: ./dataset")
+    parser.add_argument("--val_directory", type=Path,
+                        default="/home/ai/projects/vehicle-plate-recognition-training/recognition/datasets/val",
+                        help="path to the dataset, default: ./dataset")
+    parser.add_argument("--output_dir", type=Path, default="./output",
+                        help="path to the output directory, default: ./output")
+    parser.add_argument("--epochs", type=int, default=100, help="number of training epochs")
+    parser.add_argument("--device", default="cuda", help="training device, default is cuda")
+    parser.add_argument("--mean", nargs="+", type=float, default=[0.4845], help="dataset channel-wise mean")
+    parser.add_argument("--std", nargs="+", type=float, default=[0.1884], help="dataset channel-wise std")
+    parser.add_argument("--img_w", type=int, default=100, help="dataset img width size")
+    parser.add_argument("--n_workers", type=int, default=8, help="number of workers for dataloader")
+    parser.add_argument("--batch_size", type=int, default=128, help="batch size")
+    parser.add_argument("--alphabets", default='ابپتشثجدزسصطعفقکگلمنوهی+۰۱۲۳۴۵۶۷۸۹',
+                        help="alphabets used in the process")
+    parser.add_argument("--visualize", action="store_true", help="Visualize data-loader")
+
+    args = parser.parse_args()
+    config = Config()
+    config.update_config_param(args)
+
+    output_dir = mkdir_incremental(config.output_dir)
+    logger = get_logger("pytorch-lightning-image-classification", log_path=output_dir / "log.log")
+
+    early_stopping = EarlyStopping(monitor='val_loss', patience=config.early_stopping_patience)
+    model_checkpoint = ModelCheckpoint(dirpath=output_dir, filename=config.file_name,
+                                       monitor="val_loss", verbose=True)
+    learning_rate_monitor = LearningRateMonitor(logging_interval="epoch")
+
+    trainer = pl.Trainer(
+        accelerator="gpu" if config.device == "cuda" else "cpu",
+        devices=1,
+        max_epochs=config.epochs,
+        min_epochs=config.epochs // 10,
+        callbacks=[early_stopping, model_checkpoint, learning_rate_monitor],
+        default_root_dir=output_dir
+    )
+
+    model = LitCRNN(config.img_h, config.n_channels, config.n_classes,
+                    config.n_hidden, config.lstm_input, config.lr,
+                    config.lr_reduce_factor, config.lr_patience, config.min_lr)
+
+    train_loader, val_loader = get_loaders(config)
+
+    if args.visualize:
+        print("[INFO] Visualizing train-loader")
+        visualize_data_loader(train_loader, mean=config.mean, std=config.std)
+        print("[INFO] Visualizing val-loader")
+        visualize_data_loader(val_loader, mean=config.mean, std=config.std)
+        exit(0)
+
+    trainer.fit(model, train_loader, val_loader)
+
+
+if __name__ == "__main__":
+    main()
