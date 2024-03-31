99907dd3601e168749dbf3bc922d21f6f9431707
Mac <redsbaniasadi1381@gmail.com>
2024-03-31 01:06:00 +0000

    logger change

--- src/logger.py
+++ src/logger.py
@@ -1,21 +1,28 @@
 import logging
+from pathlib import Path
 
 
-def get_logger(name, log_path=None):
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+def initialize_logger(logger_name: str, file_output: str = None):
+    log = logging.getLogger(logger_name)
+    log.setLevel(logging.INFO)
 
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
+    if log.hasHandlers():
+        log.handlers.clear()
 
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_format = logging.Formatter(
+        "%(asctime)s | %(name)s | %(levelname)s | %(message)s"
+    )
+    console_handler.setFormatter(console_format)
+    log.addHandler(console_handler)
 
-    if log_path:
-        fh = logging.FileHandler(log_path)
-        fh.setLevel(logging.INFO)
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
+    if file_output:
+        log_file_path = Path(file_output)
+        log_file_path.parent.mkdir(parents=True, exist_ok=True) 
+        file_handler = logging.FileHandler(log_file_path)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(console_format)
+        log.addHandler(file_handler)
 
-    return logger
+    return log
