bb322dba5d5a1a10354271f72ecd4a5320f0fea9
Mac <redsbaniasadi1381@gmail.com>
2024-04-15 01:36:00 +0000

    add display sample and debug

--- src/visualize.py
+++ src/visualize.py
@@ -2,12 +2,16 @@ import torch
 import torchvision
 import matplotlib.pyplot as plt
 
+def display_batch_samples(data_loader, mean_vals, std_vals, max_samples=8, samples_per_row=4):
+    batch_imgs, batch_labels = next(iter(data_loader))
+    batch_imgs = batch_imgs[:max_samples]
 
-def visualize_data_loader(loader, mean, std):
-    imgs, labels = next(iter(loader))
-    imgs = imgs[:8]
-    imgs = imgs * torch.tensor(std).view(-1, 1, 1) + torch.tensor(mean).view(-1, 1, 1)
+    mean_tensor = torch.tensor(mean_vals).view(-1, 1, 1)
+    std_tensor = torch.tensor(std_vals).view(-1, 1, 1)
+    batch_imgs = batch_imgs * std_tensor + mean_tensor
 
-    grid = torchvision.utils.make_grid(imgs, nrow=4)
-    plt.imshow(grid.permute(1, 2, 0))
+    img_grid = torchvision.utils.make_grid(batch_imgs, nrow=samples_per_row)
+    plt.figure(figsize=(10, 5))
+    plt.imshow(img_grid.permute(1, 2, 0))
+    plt.axis('off')
     plt.show()
