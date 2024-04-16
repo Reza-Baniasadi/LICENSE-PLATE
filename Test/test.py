5fc433e2519a1cf1f1c9ce9162f2a87168f64aff
Mac <redsbaniasadi1381@gmail.com>
2024-04-16 21:36:00 +0000

    import all function and class project

--- Test/test.py
+++ Test/test.py
@@ -5,3 +5,10 @@ from PIL import Image
 import numpy as np
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+from dataset_module import OCRDataset, compute_mean_std, extract_text_from_filename, validate_text, prepare_dataset
+from config_module import Config
+from model_module import LitCRNN
+from dataloader_module import get_loaders
+from logger_module import initialize_logger
+from utils_module import mkdir_incremental, visualize_data_loader
+
